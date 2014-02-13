849607c49bb8b1a50cac70308223dcb2e013d38b
J. Caleb Wherry <caleb@calebwherry.com>
2014-02-13 14:48:22 -0500

    Creating build script and updating Travis-CI.
    Creating new build script in qa directory and updating .travis.yml to use
    it.
    
    Adding in CMake check for compiler versions.

---
+++ qa/build.sh
@@ -0,0 +1,215 @@
+#! /bin/bash
+########################################
+########################################
+##
+##  Copyright (C) 2014 Korovasoft, Inc.
+##
+##  File:
+##      \file build.sh
+##
+##  Description:
+##      \brief Build script.
+##
+##  Author:
+##      \author J. Caleb Wherry
+##
+########################################
+########################################
+
+
+#
+# Usage function:
+#
+usage()
+{
+	printf "Build script for Concurrent-GARTH.\n"
+	printf "\n"
+	printf "./build.sh\n"
+	printf "\t-h, --help \t Usage details.\n"
+	printf "\t-c, --clean \t Clean build files.\n"
+	printf "\n"
+}
+
+
+#
+# Filter through CLI args:
+#
+while [ "$1" != "" ]; do
+	PARAM=`echo $1 | awk -F= '{print $1}'`
+	VALUE=`echo $1 | awk -F= '{print $2}'`
+	case $PARAM in
+		-h | --help)
+    	usage
+      exit 0
+      ;;
+		-c | --clean)
+    	rm -rf build_* install_*
+			exit 0
+      ;;
+		*)
+			echo "ERROR: Unknown parameter - \"$PARAM\"."
+			usage
+      exit 1
+      ;;
+	esac
+
+	shift
+done
+
+
+#
+# Start time:
+#
+START_TIME=$(date +%s)
+
+
+#
+# Get time stamp:
+#
+TIMESTAMP="`date +\"%Y-%m-%d_%T\"`"
+
+
+#
+# Build directory name:
+#
+BUILD_DIR="build_${TIMESTAMP}"
+
+
+#
+# Install directory name:
+#
+INSTALL_DIR="install_${TIMESTAMP}"
+
+
+#
+# Logfile name:
+#
+LOGFILE="build_${TIMESTAMP}.log"
+
+
+#
+# Exit status of commands:
+#
+STATUS=0
+
+
+#
+# Create build/install directories:
+#
+echo
+echo -n "Creating build/install directories... "
+(
+mkdir -p "${BUILD_DIR}"
+mkdir -p "${INSTALL_DIR}"
+) >> ${LOGFILE} 2>&1
+STATUS=$?
+
+if [ ${STATUS} -ne 0 ]
+then
+	echo "** ERROR: Could not create build/install directories, please check ${LOGFILE}. **"
+	echo
+	cat "${LOGFILE}"
+	exit 1
+else
+	echo "done."
+fi
+
+
+#
+# CD into new build directory:
+#
+cd "${BUILD_DIR}"
+
+
+#
+# Run cmake:
+#
+echo -n "Running 'cmake'... "
+(
+cmake ../../ -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}"
+) >> ${LOGFILE} 2>&1
+STATUS=$?
+
+if [ ${STATUS} -ne 0 ]
+then
+	echo "** ERROR: 'cmake' encountered a problem, please check ${LOGFILE}! **"
+	echo
+	cat "${LOGFILE}"
+	exit 1
+else
+	echo "done."
+fi
+
+
+#
+# Run make:
+#
+echo -n "Running 'make'... "
+(
+make
+) >> ${LOGFILE} 2>&1
+STATUS=$?
+
+if [ ${STATUS} -ne 0 ]
+then
+	echo "** ERROR: 'make' encountered a problem, please check ${LOGFILE}! **"
+	echo
+	cat "${LOGFILE}"
+	exit 1
+else
+	echo "done."
+fi
+
+
+#
+# Run make install:
+#
+echo -n "Running 'make install'... "
+(
+make install
+) >> ${LOGFILE} 2>&1
+STATUS=$?
+
+if [ ${STATUS} -ne 0 ]
+then
+	echo "** ERROR: 'make install' encountered a problem, please check ${LOGFILE}! **"
+	echo
+	cat "${LOGFILE}"
+	exit 1
+else
+	echo "done."
+fi
+
+
+#
+# Build completed:
+#
+echo
+echo "Build completed successfully!"
+
+
+#
+# Do some cleanup:
+#
+cd ..
+rm -rf "${BUILD_DIR}" "${INSTALL_DIR}" ${LOGFILE}
+
+
+#
+# Stop time:
+#
+STOP_TIME=$(date +%s)
+
+
+#
+# Elapsed time:
+#
+echo
+echo "Build Execution Time: $(expr ${STOP_TIME} - ${START_TIME}) seconds."
+echo
+
+
+#
+# Exit cleanly:
+#
+exit 0

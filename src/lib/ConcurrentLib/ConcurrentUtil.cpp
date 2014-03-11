bdff13a3e6470c78e4b9fa3d07b25e9ed5eb83e5
J. Caleb Wherry <caleb@calebwherry.com>
2014-03-11 00:39:37 -0400

    Build broken, still trying to figure out wtf is wrong with using <thread> in my library instead of my app. Works fine in the app but the same code fails terribly in the lib. Probably a linking issue or some shit.

---
+++ src/lib/ConcurrentLib/ConcurrentUtil.cpp
@@ -0,0 +1,46 @@
+////////////////////////////////////////
+////////////////////////////////////////
+//
+//	Copyright (C) 2014 Korovasoft, Inc.
+//
+//  File:
+//      \file ConcurrentUtil.cpp
+//
+//  Description:
+//      \brief ConcurrentUtil Library implementation.
+//
+//  Author:
+//      \author J. Caleb Wherry
+//
+////////////////////////////////////////
+////////////////////////////////////////
+
+
+// Forward declarations:
+////
+
+
+// Local includes:
+#include "ConcurrentUtil.h"
+
+
+// Compiler includes:
+#include <thread>
+
+
+/// ConcurrentUtil namespace
+namespace ConcurrentUtil
+{
+
+	uint32_t getNumberCores()
+	{
+
+		// Initial core number from thread library:
+		uint32_t numThreads = thread::hardware_concurrency();
+
+  	// Above function could return 0, so we hardcore a value of 4 if that happens:
+  	return (numThreads != 0) ? numThreads : 4;
+	
+	}
+
+} // ConcurrentUtil namespace
---
+++ src/lib/ConcurrentLib/ConcurrentUtil.h
@@ -0,0 +1,45 @@
+////////////////////////////////////////
+////////////////////////////////////////
+//
+//	Copyright (C) 2014 Korovasoft, Inc.
+//
+//  File:
+//      \file ConcurrentUtil.h
+//
+//  Description:
+//      \brief ConcurrentUtil Library header.
+//
+//  Author:
+//      \author J. Caleb Wherry
+//
+////////////////////////////////////////
+////////////////////////////////////////
+
+
+// Include guards:
+#ifndef CONCURRENT_UTIL_H
+#define CONCURRENT_UTIL_H
+
+
+// Forward declarations:
+////
+
+
+// Local includes:
+////
+
+
+// Compiler includes:
+#include<cstdint>
+
+
+/// ConcurrentUtil namespace
+namespace ConcurrentUtil
+{
+
+	uint32_t getNumberCores();
+
+} // ConcurrentUtil namespace
+
+
+#endif // CONCURRENT_UTIL_H

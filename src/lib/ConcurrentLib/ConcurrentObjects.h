c52c8d07159adab80f2fef4399d995a5e2ad3177
J. Caleb Wherry <caleb@calebwherry.com>
2014-03-29 09:02:26 -0400

    Adding in more ConcurrentVector code.

--- src/lib/ConcurrentLib/ConcurrentObjects.h
+++ src/lib/ConcurrentLib/ConcurrentObjects.h
@@ -32,6 +32,9 @@
 // Compiler includes:
 #include <atomic>
 #include <cstdint>
+#include <vector>
+#include <thread>
+#include <mutex>
 
 
 /// ConcurrentObjects namespace
@@ -62,6 +65,63 @@ namespace ConcurrentObjects
       uint32_t getCount();
 
   };
+
+  /// ConcurrentVector data structure
+  template <typename T>
+  class ConcurrentVector
+  {
+
+    private:
+
+      std::vector<T> data;
+      std::mutex dataMutex;
+
+    public:
+
+      // Default constructor
+      ConcurrentVector();
+
+      // Custom constructor
+      ConcurrentVector(
+        uint32_t size_
+      );
+
+      // Return size
+      uint32_t size();
+
+      // Access element
+      T operator[](
+        uint32_t index
+      );
+
+      // Push back element
+      void push_back(
+        T element
+      );
+
+      // Pop back element
+      T pop_back();
+  };
+
+
+  //
+  // Template implementations for ConcurrentVector
+  //
+
+  // Default constructor
+  template <typename T>
+  ConcurrentVector<T>::ConcurrentVector():
+    data(std::vector<T>())
+  {
+  }
+
+  // Custom Constructor:
+  template <typename T>
+  ConcurrentVector<T>::ConcurrentVector (
+    uint32_t size_
+  ): data(std::vector<T>(size_))
+  {
+  }
 
 } // ConcurrentObjects namespace
 
